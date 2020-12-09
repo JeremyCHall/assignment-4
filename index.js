e5c9654e1b2867e25e47d176dbae2848febd29a5
Jeremy Hall <bynumbrooke@yahoo.com>
2020-12-08 22:46:50 -0500

    3 of 4 solutions for week fives assigment

--- index.js
+++ index.js
@@ -6,12 +6,21 @@
  //@param {number} number1
  //@param {number} number2
  //@return {number} product
-const multiplication = () => {
-  return
+ let a = parseInt(prompt("Please enter a positve whole number"));
+ let b = parseInt(prompt("Please enter second positive whole number"));
+ const multiplication = (a,b) => {
+   console.log(a)
+   console.log(b)
+  let product =0;
+   for(let i =1; i <= b; i++){
+    console.log("counter", i)
+    product+=a;
+   }
+  return product
 }
 
-const answer1 = multiplication()
-console.log('answer1', answer1)
+const answer1 = multiplication(a,b)
+console.log('answer1')
 
 const htmlTarget = document.getElementById('a-1')
 htmlTarget.innerHTML = answer1
@@ -25,15 +34,53 @@ htmlTarget.innerHTML = answer1
 
 //@param {string}
 //@return {character} => array of characters
-vowelOrConsonant = () => {
-  return
+
+//https://stackoverflow.com/questions/57503603/while-loop-to-print-vowels-and-other-elements-on-a-new-line-in-javascript
+//let vowelsOrConsonants=prompt(`Please enter //a string.`)
+var introduce=(prompt(`Please list the states you have lived in with no commas.`));
+function vowelOrConsonants(str) {
+  
+  var vo=[];
+  var co=[];
+ // const vowels=['a','e','i','o','u'];
+
+  var split = str.split("");
+
+  for(let i=0; i<split.length; i++){
+    if (split[i] ==="a"||split[i] ==="A"){
+      vo.push(split[i]);}
+    else if (split[i] ==="e"||split[i] ==="E"){
+      vo.push(split[i]);}
+    else if (split[i] ==="i"||split[i] ==="I"){
+      vo.push(split[i]);}
+    else if (split[i] ==="o"||split[i] ==="O"){
+      vo.push(split[i]);}
+    else if (split[i] ==="u"||split[i] ==="U"){
+      vo.push(split[i]);}
+    else {
+      co.push(split[i]);
+      //console.log(i);
+    }
+  }
+  console.log(co);
+  console.log(vo);
+  let voc=prompt(`Do you want vowels or consonants first?  Enter v or c.`)
+var consoFirst = co.concat(vo);
+var vowelsFirst=vo.concat(co);
+if (voc===`v`){
+alert (vowelsFirst);}
+else if(voc===`c`){
+  alert (consoFirst);
+}
+else{
+  alert (`You should have entered v or c`);
 }
 
-const answer2 = vowelOrConsonant()
+}
+//console.log (vo+"vowels");
+//console.log(co+"consonants");
 
-const htmlTarget2 = document.getElementById('a-2')
-htmlTarget2.innerHTML = answer2
-
+vowelOrConsonants(introduce.split(" ").join(""));
 
 
 
@@ -45,7 +92,43 @@ htmlTarget2.innerHTML = answer2
 //where: name, saves the name of the player. Lives, represents the remaining oportunities each time the player fails. Fail_numbers, is an array of numbers that stores the fail numbers the player has used
 
 //@return {string} win / gameOver => the string that says if the user wasted the three oportunities showing the fails numbers or the name if the player wins
-guessTheNumber  = () => {
+let userName = prompt("Hello, whats your name?");
+let playerInfo = {
+  name: userName,
+  lives: 3,
+  failNumbers: 0
+}
+
+// generate random number between 10 and 50
+let randomGenerator = Math.floor(Math.random() * 41) + 10
+//console.log(randomGenerator);
+
+// loop to keep the fun games going
+for (var i = 0; i < 3; i++ ) {
+  let userInput = prompt('Guess a number betwen 10 and 50.');
+  if (+userInput === randomGenerator) {
+    alert (`Woo! ${playerInfo[`name`]} won the game!`);
+    break;
+  } 
+  else if (+userInput !== randomGenerator) {
+    userInput;
+    playerInfo['lives']--;
+    playerInfo[`failNumbers`]++;
+    //console.log(playerInfo['lives']);
+    //console.log(playerInfo[`failNumbers`]);
+  }
+   
+  if (playerInfo['failNumbers'] == 1) {
+  alert (`Incorrect guess.  You have 2 lives left.`);
+  }
+  else if (playerInfo['failNumbers'] == 2) {
+  alert (`Wrong again.  You only have 1 life left.`);
+  }
+  else if (playerInfo['failNumbers'] == 3) 
+  alert (`I'm sorry.  You have no lives left.  You lose.`);   
+  }
+
+/*guessTheNumber  = () => {
   return
 }
 
@@ -53,8 +136,9 @@ const answer3 = guessTheNumber()
 
 const htmlTarget3 = document.getElementById('a-3')
 htmlTarget3.innerHTML = answer3
+*/
 
-
+  
 
 //------------------------ Question 4 ---------------------------
 // In the function below we are giving you an array of objects, each one with the same properties. Ask to the user for 3 diferentes options to sorting the array from the highest to lowest. In the case of a string, the criteria to sort must be the length of the string. The first one is sorting the array of objects based on the title property.
@@ -87,3 +171,4 @@ const answer4 = sort()
 
 const htmlTarget4 = document.getElementById('a-4')
 htmlTarget4.innerHTML = answer4
+}
